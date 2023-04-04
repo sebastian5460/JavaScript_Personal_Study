63e321b5a7547f12b34b26814d47d101d218257e
John020 <sebastianchamorro271@gmail.com>
2023-04-04 07:35:49 -0500

    new Projects

---
+++ BasicMaths/consoleGroups.js
@@ -0,0 +1,19 @@
+
+function calculateSquare(side){
+
+    
+    console.log({
+        "Perimeter": side * 4,
+        "Area": (side * side)
+    })
+    
+}
+
+
+// let a = console.log("Enter the area of the square: ");
+
+// calculateSquare(a);
+
+
+
+
